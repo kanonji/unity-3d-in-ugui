7f35f569411f6a03b4ffc4f6f36fe9bc9f3285d2
kanonji <kanonji@users.noreply.github.com>
2016-06-23 13:40:30 +0900

    Reimplement with EventTrigger

---
+++ Assets/Scripts/RotateByTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotateByTrigger : MonoBehaviour {
+	[SerializeField]
+	private float rotateSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Rotate(BaseEventData baseEvent)
+    {
+        PointerEventData pointerEvent = baseEvent as PointerEventData;
+        float angleY = pointerEvent.delta.x * rotateSpeed;
+        transform.Rotate(new Vector3(transform.eulerAngles.x, angleY, 0));
+    }
+}
